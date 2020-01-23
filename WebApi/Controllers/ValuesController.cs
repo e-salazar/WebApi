5717b484b3117bdee04133010851bd1a97eb9861
Eduardo Salazar <eduardo.salazar@outlook.com>
2020-01-23 15:30:22 -0300

    Agregar archivos de proyecto.

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            this._logger.LogInformation("Endpoint Called");
+            return new[] { 1, 2, 3 };
+        }
+    }
+}
